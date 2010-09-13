1ab8e2354c1607f2d7243dac696de7df7d62a201
Simon de Haan <simon@soocial.com>
2010-09-13 19:43:46 +0200

    tracking changes with simple versioning and a m2m model

--- example_project/app/models.py
+++ example_project/app/models.py
@@ -1,24 +1,78 @@
 from django.db import models
+from django.db.models import F
+from dirtyfields import DirtyFieldsMixin
+from history.models import HistoricalRecords
 
-class Node(models.Model):
+class Node(DirtyFieldsMixin, models.Model):
+    """
+    A node is an entity in the network to which any associated data can be
+    pushed. In this contrived example, a Clip is the only associated data
+    possible.
+    """
     title = models.CharField(max_length=255)
     clips = models.ManyToManyField('Clip', through='Propagation')
+    active = models.BooleanField(default=True)
+    # internal MVCC
     version = models.IntegerField(default=0)
+    history = HistoricalRecords()
+    
+    def __unicode__(self):
+        return "<Node %s@v%s>" % (self.title, self.version)
     
     def save(self, *args, **kwargs):
-        self.version += 1
-        super(Node, self).save(*args, **kwargs)
+        if self.is_dirty():
+            super(Node, self).save(*args, **kwargs)
+            # update the versioning based on the DBs current value of the field
+            # hopefully avoid some nasty race conditions
+            Node.objects.filter(pk=self.pk).update(version=F('version') + 1)
+            # Update the propagation object, have Django do the DB lookup through
+            # a join and fill in that value.
+        
+            # Here be dragons
+            saved_node = Node.objects.get(pk=self.pk)
+            Propagation.objects.filter(node=self).update(master_version=saved_node.version)
+
+class PropagationManager(models.Manager):
+    def to_push(self):
+        return self.get_query_set().extra(where=["master_version > slave_version"])
+    
+    def to_pull(self):
+        return self.get_query_set().extra(where=["slave_version > master_version"])
 
 class Propagation(models.Model):
     node = models.ForeignKey('Node')
     clip = models.ForeignKey('Clip')    
+    # keep track of which version is where.
     master_version = models.IntegerField(default=0)
     slave_version = models.IntegerField(default=0)
+    
+    objects = models.Manager()
+    changes = PropagationManager()
+    
+    @classmethod
+    def summary_of_changes(klass):
+        for change in klass.changes.to_push():
+            master_version = change.node
+            historical_slave_version = master_version.history.get(version=change.slave_version)
+            slave_version = historical_slave_version.history_object
+            print "Got a change:"
+            print "\tMaster version number is: %s" % change.master_version
+            print "\tMaster field values: %s" % master_version._as_dict()
+            print "\tSlave version number is: %s" % change.slave_version
+            print "\tSlave field values: %s" % slave_version._as_dict()
+        
 
 class Clip(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    active = models.BooleanField(default=True)
+    # internal MVCC
     version = models.IntegerField(default=0)
+    
+    history = HistoricalRecords()
+    
+    def __unicode__(self):
+        return "<Clip %s@v%s>" % (self.title, self.version)
     
     def save(self, *args, **kwargs):
         self.version += 1
--- example_project/app/tests.py
+++ example_project/app/tests.py
@@ -1,18 +1,60 @@
 from django.test import TestCase
 from app.models import Node, Clip, Propagation
 
+
 class AppTestCase(TestCase):
     
-    def setUp(self):
-        pass
+    def create_nodes_and_clips(self):
+        self.node = Node(title="Node 1")
+        self.node.save()
+        
+        self.clip1 = Clip(title="Clip 1")
+        self.clip1.save()
+        self.clip2 = Clip(title="Clip 2")
+        self.clip2.save()
+        
+        # This setup requires explicite creation of M2M objects
+        # node.clips.create & node.clips.add doesn't work
+        Propagation.objects.create(node=self.node, clip=self.clip1)
+        Propagation.objects.create(node=self.node, clip=self.clip2)
+        
+        # reload these manually, tests are throwing things off
+        self.node = Node.objects.get(pk=self.node.pk)
+        self.clip1 = Clip.objects.get(pk=self.clip1.pk)
+        self.clip2 = Clip.objects.get(pk=self.clip2.pk)
+        
     
-    def tearDown(self):
-        pass
+    def test_dirtyfields(self):
+        """Make sure the dirty fields actually work"""
+        self.create_nodes_and_clips()
+        self.node.active = False
+        self.assertTrue(self.node.is_dirty())
+        # check the dirty fields, returning the original state as in the DB
+        self.assertEquals(self.node.get_dirty_fields(), {'active': True})
     
     def test_propagation_tracking(self):
-        node = Node.objects.create(title="Node 1")
-        clip1 = Clip.objects.create(title="Clip 1")
-        clip2 = Clip.objects.create(title="Clip 2")
+        """Check the propagation of changes from node to clips"""
+        self.create_nodes_and_clips()
+        self.assertEquals(Propagation.changes.to_push().count(), 0)
         
-        Propagation.objects.create(node=node, clip=clip1)
-        Propagation.objects.create(node=node, clip=clip2)
+        self.node.title="Changed title"
+        self.node.save()
+        
+        # the clips versions are behind and should be pushed
+        self.assertEquals(Propagation.changes.to_push().count(), 2)
+        
+    def test_no_propagation_when_nothings_changed(self):
+        """Nothing should be changed if no fields have changed"""
+        self.create_nodes_and_clips()
+        self.assertFalse(self.node.is_dirty())
+        self.node.save()
+        self.assertEquals(Propagation.changes.to_push().count(), 0)
+    
+    def test_changes_being_propagated(self):
+        """Using the History we should be able to see what's changed"""
+        self.create_nodes_and_clips()
+        self.node.title="Changed Title"
+        self.node.save()
+        
+        print Propagation.summary_of_changes()
+        
